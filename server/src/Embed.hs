842a2d4aee5f13c33e64b1d5349cccabad244c73
George Thomas <georgefsthomas@gmail.com>
2020-05-06 01:11:21 +0100

    Add makefile.

--- server/src/Embed.hs
+++ server/src/Embed.hs
@@ -7,5 +7,5 @@ import Data.Text.Encoding (decodeUtf8)
 
 mainCSS, elmJS, jsJS :: Text
 mainCSS = decodeUtf8 $(embedFile "../client/manual/main.css")
-elmJS = decodeUtf8 $(embedFile "../client/dist/elm.js")
+elmJS = decodeUtf8 $(embedFile "../dist/elm.js")
 jsJS = decodeUtf8 $(embedFile "../client/manual/js.js")

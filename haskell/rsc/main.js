5d69f7cd7145c4d7adb9da67bafb30af67e6ed69
George Thomas <georgefsthomas@gmail.com>
2020-05-09 17:54:16 +0100

    Switch to fullscreen when in landscape.
    Currently only seems to work in Chrome, but that's where we really needed it anyway.

--- haskell/rsc/main.js
+++ haskell/rsc/main.js
@@ -1,3 +1,20 @@
+// TODO Firefox doesn't yet give permission to trigger fullscreen from an orientation change
+// https://github.com/whatwg/fullscreen/issues/34
+// though Firefox actually works kind of alright without fullscreen anyway (Chrome really doesn't)
+window.screen.orientation.onchange = function () {
+    console.log(this.type)
+    switch (this.type) {
+        case "landscape-primary":
+        case "landscape-secondary":
+            document.documentElement.requestFullscreen();
+            break;
+        default:
+            //TODO this errors if we're not fullscreen to begin with, but does that actually matter?
+            document.exitFullscreen();
+            break;
+    };
+}
+
 var username = document.currentScript.getAttribute("username");
 
 var wsAddress = document.currentScript.getAttribute("wsAddress");
@@ -5,7 +22,7 @@ var wsAddress = document.currentScript.getAttribute("wsAddress");
 var ws = new WebSocket(wsAddress);
 
 //TODO waiting on this before even initialising Elm ensures no race condition, but is it a bit slow?
-    // also what if the websocket can't be opened? we shoud time out
+// also what if the websocket can't be opened? we should time out
 ws.onopen = function (event) {
     ws.send(username);
 

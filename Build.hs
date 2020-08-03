444ac2471c6ee3e3ca81d87d7f62e5307eeca257
George Thomas <georgefsthomas@gmail.com>
2020-08-04 00:23:39 +0100

    Use default Shake directory

--- Build.hs
+++ Build.hs
@@ -31,7 +31,7 @@ import Language.JavaScript.Parser (parse, renderToText)
 import Language.JavaScript.Process.Minify (minifyJS)
 
 main :: IO ()
-main = shakeArgs shakeOptions {shakeFiles = build} $ do
+main = shakeArgs shakeOptions $ do
     want [monpad]
 
     "dhall" ~> need [dhall]

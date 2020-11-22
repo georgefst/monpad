georgefst/monpad

commit 444ac2471c6ee3e3ca81d87d7f62e5307eeca257

Use default Shake directory

Build.hs
@@ -31,7 +31,7 @@ import Language.JavaScript.Parser (parse, renderToText)
 import Language.JavaScript.Process.Minify (minifyJS)
 
 main :: IO ()
-main = shakeArgs shakeOptions {shakeFiles = build} $ do
+main = shakeArgs shakeOptions $ do
     want [monpad]
 
     "dhall" ~> need [dhall]

.gitignore
@@ -1,4 +1,4 @@
-.build*/
+.shake/
 dist*/
 elm-stuff*/
 .ghc.environment.*

commit 0712a66fec788baf8bf9066bbdae9e0237e2af00

Correct shake directory
'clean' has been broken since 444ac24.

Build.hs
@@ -38,7 +38,7 @@ main = shakeArgs shakeOptions{shakeColor = True, shakeThreads = 0} $ do
     "elm" ~> need [elm]
     let clean = do
             putInfo "Cleaning Shake build artefacts"
-            removeFilesAfter build ["//*"]
+            removeFilesAfter shakeDir ["//*"]
             putInfo "Cleaning generated assets"
             removeFilesAfter rscDistDir ["//*"]
             removeFilesAfter distDir ["//*"]
@@ -70,8 +70,8 @@ main = shakeArgs shakeOptions{shakeColor = True, shakeThreads = 0} $ do
 monpad :: FilePath
 monpad = distDir </> "monpad" <.> exe
 
-build :: FilePath
-build = ".build"
+shakeDir :: FilePath
+shakeDir = ".shake"
 
 distDir :: FilePath
 distDir = "dist"
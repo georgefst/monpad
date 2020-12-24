d37e67a9eab786e632c3e6ea0e338c3379bea7de
George Thomas <georgefsthomas@gmail.com>
2020-12-24 12:08:46 +0000

    Remove all JS semicolons
    They're less necessary than I thought - just don't start a line with ( or [.

--- js/main.js
+++ js/main.js
@@ -4,28 +4,28 @@ window.screen.orientation.onchange = () => {
     switch (this.type) {
         case "landscape-primary":
         case "landscape-secondary":
-            document.documentElement.requestFullscreen();
-            break;
+            document.documentElement.requestFullscreen()
+            break
         default:
-            document.exitFullscreen();
-            break;
-    };
-};
+            document.exitFullscreen()
+            break
+    }
+}
 
-const attr = s => document.currentScript.attributes.getNamedItem(s).value;
+const attr = s => document.currentScript.attributes.getNamedItem(s).value
 
-const username = attr("username");
+const username = attr("username")
 
-const wsAddress = "ws://" + location.hostname + ":" + attr("wsPort") + "?" + new URLSearchParams({ username });
-const ws = new WebSocket(wsAddress);
+const wsAddress = "ws://" + location.hostname + ":" + attr("wsPort") + "?" + new URLSearchParams({ username })
+const ws = new WebSocket(wsAddress)
 
-const layout = JSON.parse(attr("layout"));
+const layout = JSON.parse(attr("layout"))
 
-window.onbeforeunload = () => ws.close();
+window.onbeforeunload = () => ws.close()
 
 ws.onopen = event => {
     const app = Elm.Main.init({
         flags: { username, layout }
-    });
-    app.ports.sendUpdatePort.subscribe(message => ws.send(JSON.stringify(message)));
-};
+    })
+    app.ports.sendUpdatePort.subscribe(message => ws.send(JSON.stringify(message)))
+}

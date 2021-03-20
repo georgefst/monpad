8bb84585b8a0905dc4869598758bc32431c537fc
George Thomas <georgefsthomas@gmail.com>
2021-03-20 11:10:37 +0000

    Clean up JS

--- js/main.js
+++ js/main.js
@@ -9,39 +9,48 @@ const layouts = JSON.parse(attr("layouts"))
 
 window.onbeforeunload = () => ws.close()
 
-ws.onopen = event => {
+ws.onopen = _event => {
+    // Elm
     elmInitialised = false
     const app = Elm.Main.init({
         flags: { username, layouts }
     })
     //TODO this (waiting for an initialisation message)
     // is all just a workaround for the fact that Elm has no built-in way of signalling that 'init' has finished
-    app.ports.initPort.subscribe(message => {
+    app.ports.initPort.subscribe(() => {
         elmInitialised = true
         dispatchEvent(new Event("elmInit"))
     })
-    app.ports.fullscreenPort.subscribe(message => {
+
+    // fullscreen
+    app.ports.fullscreenPort.subscribe(() => {
         document.documentElement.requestFullscreen()
     })
-    document.onfullscreenchange = event => {
+    document.onfullscreenchange = _event => {
         app.ports.fullscreenChangePort.send(
             !(document.fullscreenElement == null)
         )
     }
+
+    // update messages
     app.ports.sendUpdatePort.subscribe(message => ws.send(JSON.stringify(message)))
     ws.addEventListener("message", event => {
         const send = () => app.ports.receiveUpdatePort.send(JSON.parse(event.data))
         if (elmInitialised) {
             send()
         } else {
-            addEventListener("elmInit", event => {
+            addEventListener("elmInit", () => {
                 setTimeout(send, 0)
             })
         }
     })
+
+    // audio
     app.ports.audioPort.subscribe(url => {
         audio = new Audio(url)
         audio.play()
     })
-    app.ports.logPort.subscribe(message => console.log(message))
+
+    // logging
+    app.ports.logPort.subscribe(console.log)
 }

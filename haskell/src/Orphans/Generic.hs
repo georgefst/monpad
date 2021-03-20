e54087425d6c799105907b14284940b37655130e
George Thomas <georgefsthomas@gmail.com>
2021-03-20 13:30:04 +0000

    Add some Generic instances

---
+++ haskell/src/Orphans/Generic.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+-- | These are mostly/entirely for the sake of using `generic-optics`.
+module Orphans.Generic where
+
+import GHC.Generics (Generic)
+import Network.WebSockets.Connection qualified as WS
+
+deriving instance Generic WS.PendingConnection
+deriving instance Generic WS.ConnectionOptions

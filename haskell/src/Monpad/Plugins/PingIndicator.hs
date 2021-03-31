9c5722b223256ba73e1848f45e56032b5940f7e1
George Thomas <georgefsthomas@gmail.com>
2021-03-31 20:16:55 +0100

    Minor refactor
    Maths.

--- haskell/src/Monpad/Plugins/PingIndicator.hs
+++ haskell/src/Monpad/Plugins/PingIndicator.hs
@@ -13,8 +13,7 @@ showPing vb =
     let onNewConnection = const $ pure (mempty, mempty, [initialUpdate])
         onPong = const \time -> pure
             let okPing = 1 / 10 -- time in seconds to map to 0.5 goodness
-                scaleFactor = negate $ log 0.5 / okPing
-                goodness = exp $ negate (realToFrac time) * scaleFactor -- in range (0, 1]
+                goodness = 0.5 ** (realToFrac time / okPing) -- in range (0, 1]
             in  [ SetText elementId $ showT time
                 , SetIndicatorColour elementId $ Colour (1 - goodness) goodness 0 1
                 ]

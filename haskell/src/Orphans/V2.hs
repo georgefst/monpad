2ed5f49652c9558e63ca351bf18650ba71fac353
George Thomas <georgefsthomas@gmail.com>
2021-04-16 16:26:54 +0100

    Simplify deriving Dhall and JSON instances for vectors
    The only reason we weren't doing out this way before is bacuse the Elm deriving adds the variables to the type name, but now that's been split out anyway.

--- haskell/src/Orphans/V2.hs
+++ haskell/src/Orphans/V2.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 -- | Instances for working with JSON and Dhall.
@@ -10,34 +9,24 @@ import Dhall (FromDhall (autoWith))
 import GHC.Generics (Generic)
 import Linear.V2 (V2 (V2))
 
-data IntVec2 = IntVec2
-    { x :: Int
-    , y :: Int
-    }
-    deriving (Generic, FromDhall, ToJSON)
-data WordVec2 = WordVec2
-    { x :: Word
-    , y :: Word
-    }
-    deriving (Generic, FromDhall, ToJSON)
-data DoubleVec2 = DoubleVec2
-    { x :: Double
-    , y :: Double
+data Vec2 a = Vec2
+    { x :: a
+    , y :: a
     }
     deriving (Generic, FromDhall, ToJSON)
 
 instance ToJSON (V2 Int) where
-    toJSON = J.toJSON . \(V2 x y) -> IntVec2 x y
+    toJSON = J.toJSON . \(V2 x y) -> Vec2 x y
 instance ToJSON (V2 Word) where
-    toJSON = J.toJSON . \(V2 x y) -> WordVec2 x y
+    toJSON = J.toJSON . \(V2 x y) -> Vec2 x y
 instance ToJSON (V2 Double) where
-    toJSON = J.toJSON . \(V2 x y) -> DoubleVec2 x y
+    toJSON = J.toJSON . \(V2 x y) -> Vec2 x y
 
 instance FromDhall (V2 Int) where
-    autoWith = fmap (\(IntVec2 x y) -> V2 x y) . autoWith
+    autoWith = fmap (\(Vec2 x y) -> V2 x y) . autoWith
 instance FromDhall (V2 Word) where
-    autoWith = fmap (\(WordVec2 x y) -> V2 x y) . autoWith
+    autoWith = fmap (\(Vec2 x y) -> V2 x y) . autoWith
 instance FromDhall (V2 Double) where
-    autoWith = fmap (\(DoubleVec2 x y) -> V2 x y) . autoWith
+    autoWith = fmap (\(Vec2 x y) -> V2 x y) . autoWith
 
 deriving instance FromJSON (V2 Double)

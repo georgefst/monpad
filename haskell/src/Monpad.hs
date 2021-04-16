georgefst/monpad

commit 2b7171831a275c238b0aa54cfd549f9385f7f1dd

Unify common parts of handling client and server updates in backend
We also revert the passing of layout to the server config (1ff22cc), since it's already in the Monpad monad's environment.

Combining configs with different state/env parameters is broken for now, but we'll fix that in a forthcoming commit.

haskell/src/Monpad.hs
@@ -5,11 +5,11 @@ module Monpad (
     serverExtWs,
     Monpad,
     runMonpad,
-    ServerConfig,
-    ServerConfig' (..),
+    ServerConfig (..),
     combineConfs,
     ClientID (..),
     Update (..),
+    ClientUpdate (..),
     ServerUpdate (..),
     V2 (..),
     Unit (..),
@@ -20,8 +20,12 @@ module Monpad (
 
 import Control.Concurrent
 import Control.Exception
+import Control.Monad.Base
+import Control.Monad.Catch (MonadThrow)
+import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Trans.Control
 import Data.Aeson (FromJSON, ToJSON, eitherDecode, encode)
 import Data.Aeson qualified as J
 import Data.Aeson.Text (encodeToLazyText)
@@ -31,6 +35,7 @@ import Data.IORef
 import Data.List.NonEmpty qualified as NE
 import Data.Map (Map, (!?))
 import Data.Map qualified as Map
+import Data.Maybe
 import Data.Proxy
 import Data.Semigroup.Monad
 import Data.Text (Text)
@@ -77,15 +82,21 @@ newtype ClientID = ClientID Text
     deriving (Eq, Ord, Show)
     deriving newtype FromHttpApiData
 
+data Update a b
+    = ClientUpdate ClientUpdate
+    | ServerUpdate (ServerUpdate a b)
+    deriving (Show)
+
 -- | A message sent by a client.
-data Update
+data ClientUpdate
     = ButtonUp ElementID
     | ButtonDown ElementID
     | StickMove ElementID (V2 Double) -- always a vector within the unit circle
     | SliderMove ElementID Double -- between 0 and 1
     deriving (Eq, Ord, Show, Generic, SOP.Generic, SOP.HasDatatypeInfo)
-    deriving (FromJSON, HasElmType, HasElmEncoder J.Value) via Elm.Via Update
+    deriving (FromJSON, HasElmType, HasElmEncoder J.Value) via Elm.Via ClientUpdate
 
+-- | A message generated by the server.
 data ServerUpdate a b
     = PlayAudioURL Text
     | Vibrate [Int]
@@ -181,19 +192,31 @@ mainHtml layouts wsPort (ClientID username) = doctypehtml_ $ mconcat
 --TODO use dedicated record types for State and Reader, and expose a cleaner interface
 -- | The Monpad monad
 newtype Monpad e s a b x = Monpad
-    {unMonpad :: ReaderT (Map LayoutID (Layout a b), e, ClientID) (StateT (Layout a b, ElementMaps a b, s) IO) x}
+    { unMonpad ::
+        ReaderT
+            (Map LayoutID (Layout a b), e, ClientID)
+            ( StateT
+                (Layout a b, ElementMaps a b, s)
+                (ExceptT MonpadException IO)
+            )
+            x
+    }
     deriving newtype
         ( Functor
         , Applicative
         , Monad
         , MonadIO
+        , MonadThrow
+        , MonadBase IO
+        , MonadBaseControl IO
         , MonadReader (Map LayoutID (Layout a b), e, ClientID)
         , MonadState (Layout a b, ElementMaps a b, s)
+        , MonadError MonpadException
         )
 deriving via Mon (Monpad e s a b) x instance Semigroup x => (Semigroup (Monpad e s a b x))
 deriving via Mon (Monpad e s a b) x instance Monoid x => (Monoid (Monpad e s a b x))
-runMonpad :: Layouts a b -> ClientID -> e -> s -> Monpad e s a b x -> IO x
-runMonpad ls c e s mon = evalStateT
+runMonpad :: Layouts a b -> ClientID -> e -> s -> Monpad e s a b x -> IO (Either MonpadException x)
+runMonpad ls c e s mon = runExceptT $ evalStateT
     (runReaderT
         (unMonpad mon)
         (Map.fromList . NE.toList $ ((.name) &&& id) <$> ls, e, c)
@@ -207,45 +230,40 @@ data MonpadException = WebSocketException WS.ConnectionException | UpdateDecodeE
 getting rid of some 'asyncly', 'serially' boilerplate
 -}
 -- | `e` is a fixed environment. 's' is an updateable state.
-type ServerConfig e s a b = Layouts a b -> ServerConfig' e s a b
-data ServerConfig' e s a b = ServerConfig
+data ServerConfig e s a b = ServerConfig
     { onStart :: Text -> IO () -- takes url
-    , onNewConnection :: ClientID -> IO (e, s, [ServerUpdate a b])
-    , onMessage :: Update -> Monpad e s a b [ServerUpdate a b]
+    , onNewConnection :: Layouts a b -> ClientID -> IO (e, s, [ServerUpdate a b])
+    , onDroppedConnection :: ClientID -> MonpadException -> e -> IO ()
     , onAxis :: a -> Double -> Monpad e s a b [ServerUpdate a b]
     -- ^ the argument here always ranges from -1 to 1, even for sliders
     , onButton :: b -> Bool -> Monpad e s a b [ServerUpdate a b]
-    , onDroppedConnection :: MonpadException -> Monpad e s a b [ServerUpdate a b]
     , onPong :: e -> NominalDiffTime -> IO [ServerUpdate a b]
     -- ^ when the client sends a pong, this gives us the time since the corresponding ping
     , updates :: e -> Async (s -> [ServerUpdate a b])
-    , onUpdate :: ServerUpdate a b -> Monpad e s a b [ServerUpdate a b]
+    , onUpdate :: Update a b -> Monpad e s a b [ServerUpdate a b]
     -- ^ we need to be careful not to cause an infinite loop here, by always generating new events we need to respond to
     }
     deriving Generic
-    deriving (Semigroup, Monoid) via Generically (ServerConfig' e s a b)
+    deriving (Semigroup, Monoid) via Generically (ServerConfig e s a b)
 
 -- | Run two `ServerConfig`s with different states and environments.
 combineConfs :: ServerConfig e1 s1 a b -> ServerConfig e2 s2 a b -> ServerConfig (e1, e2) (s1, s2) a b
-combineConfs sc1' sc2' ls = ServerConfig
+combineConfs sc1 sc2 = ServerConfig
     { onStart = pure (<>)
         <*> sc1.onStart
         <*> sc2.onStart
-    , onNewConnection = pure (pure \(e1, s1, u1) (e2, s2, u2) -> ((e1, e2), (s1, s2), u1 ++ u2))
-        <<*>> sc1.onNewConnection
-        <<*>> sc2.onNewConnection
-    , onMessage = pure f
-        <*> sc1.onMessage
-        <*> sc2.onMessage
+    , onNewConnection = pure (pure (pure \(e1, s1, u1) (e2, s2, u2) -> ((e1, e2), (s1, s2), u1 ++ u2)))
+        <<<*>>> sc1.onNewConnection
+        <<<*>>> sc2.onNewConnection
+    , onDroppedConnection = pure (pure \f1 f2 (e1, e2) -> f1 e1 >> f2 e2)
+        <<*>> sc1.onDroppedConnection
+        <<*>> sc2.onDroppedConnection
     , onAxis = pure (pure f)
         <<*>> sc1.onAxis
         <<*>> sc2.onAxis
     , onButton = pure (pure f)
         <<*>> sc1.onButton
         <<*>> sc2.onButton
-    , onDroppedConnection = pure f
-        <*> sc1.onDroppedConnection
-        <*> sc2.onDroppedConnection
     , onPong = \(e1, e2) -> pure (<>)
         <*> sc1.onPong e1
         <*> sc2.onPong e2
@@ -258,18 +276,9 @@ combineConfs sc1' sc2' ls = ServerConfig
         <*> sc2.onUpdate
     }
   where
-    sc1 = sc1' ls
-    sc2 = sc2' ls
     f :: Monoid x => Monpad ex sx a b x -> Monpad ey sy a b x -> Monpad (ex, ey) (sx, sy) a b x
-    f x y = do
-        (ml, (ex, ey), c) <- ask
-        (l0, me0, (sx, sy)) <- get
-        let rx = runReaderT (unMonpad x) (ml, ex, c)
-            ry = runReaderT (unMonpad y) (ml, ey, c)
-        (ax, (l1, me1, sx')) <- liftIO $ runStateT rx (l0, me0, sx)
-        (ay, (l2, me2, sy')) <- liftIO $ runStateT ry (l1, me1, sy)
-        put (l2, me2, (sx', sy'))
-        pure $ ax <> ay
+    f x y = undefined
+
 
 -- | Maps of element names to axes and buttons.
 data ElementMaps a b = ElementMaps
@@ -302,7 +311,7 @@ uniqueNames ls = flip evalState allNames $ for ls \l ->
     err = error "broken invariant in `uniqueNames` - all names should be in map by construction"
 
 server :: Int -> Port -> Maybe Text -> Maybe FilePath -> Layouts a b -> ServerConfig e s a b -> IO ()
-server pingFrequency port loginImage assetsDir (uniqueNames -> layouts) (($ layouts) -> conf) = do
+server pingFrequency port loginImage assetsDir (uniqueNames -> layouts) conf = do
     onStart conf =<< serverAddress port
     run port . serve (Proxy @(HttpApi :<|> WsApi)) $
         httpServer port loginImage assetsDir layouts :<|> websocketServer pingFrequency layouts conf
@@ -331,13 +340,13 @@ httpServer wsPort loginImage assetsDir layouts =
             serveDirectoryWebApp
             assetsDir
 
-websocketServer :: Int -> Layouts a b -> ServerConfig' e s a b -> Server WsApi
+websocketServer :: Int -> Layouts a b -> ServerConfig e s a b -> Server WsApi
 websocketServer pingFrequency layouts ServerConfig{..} mu pending0 = liftIO case mu of
     Nothing -> T.putStrLn ("Rejecting WS connection: " <> err) >> WS.rejectRequest pending0 (encodeUtf8 err)
       where err = "no username parameter"
     Just clientId -> do
         lastPing <- newIORef Nothing
-        (e, s0, u0) <- onNewConnection clientId
+        (e, s0, u0) <- onNewConnection layouts clientId
         extraUpdates <- newEmptyMVar
         let onPing = writeIORef lastPing . Just =<< getPOSIXTime
             onPong' = readIORef lastPing >>= \case
@@ -347,54 +356,55 @@ websocketServer pingFrequency layouts ServerConfig{..} mu pending0 = liftIO case
                     putMVar extraUpdates =<< onPong e (t1 - t0)
             pending = pending0 & (#pendingOptions % #connectionOnPong) %~ (<> onPong')
         conn <- WS.acceptRequest pending
-        let stream = asyncly $
-                (Left <$> SP.cons (const u0) (updates e) <> SP.repeatM (const <$> takeMVar extraUpdates))
-                    <> (Right <$> serially (SP.repeatM $ getUpdate conn))
+        let allUpdates = asyncly $ (pure . ClientUpdate <$> clientUpdates) <> (ServerUpdate <<$>> serverUpdates)
+            clientUpdates = serially . SP.repeatM $ getUpdate conn
+            serverUpdates = serially . SP.mapM (gets thd3 <&>) . SP.hoist liftIO $
+                SP.cons (const u0) (asyncly $ updates e) <> SP.repeatM (const <$> takeMVar extraUpdates)
             handleUpdates = sendUpdates conn . map (bimap (const Unit) (const Unit)) . concat <=< traverse (go . pure)
               where
                 go us = if null us
                     then mempty
-                    else fmap (us ++) $ go . concat =<< for us \u -> do
+                    else fmap (sus ++) $ go . map ServerUpdate . concat =<< for us \u -> do
                         s <- gets thd3
-                        case u of
-                            SetLayout l -> put (l, mkElementMaps l.elements, s)
-                            SwitchLayout i -> asks ((!? i) . fst3) >>= \case
-                                Just l -> put (l, mkElementMaps l.elements, s)
-                                Nothing -> warn $ "layout id not found: " <> i.unwrap
-                            AddElement el -> modify . first $ addToElementMaps el
-                            RemoveElement el -> modify . first $
-                                over #stickMap (Map.delete el)
-                                    . over #sliderMap (Map.delete el)
-                                    . over #buttonMap (Map.delete el)
-                            _ -> mempty
-                        onUpdate u
-        WS.withPingThread conn pingFrequency onPing . runMonpad layouts clientId e s0 . SP.drain $
-            flip SP.takeWhileM (SP.hoist liftIO stream) \case
-                Left sus -> do
-                    s <- gets thd3
-                    handleUpdates $ sus s
-                    pure True
-                Right (Right u) -> do
-                    ElementMaps{..} <- gets snd3
-                    handleUpdates =<< onMessage u
-                    handleUpdates =<< case u of
-                        ButtonUp t -> lookup' buttonMap t $ flip onButton False
-                        ButtonDown t -> lookup' buttonMap t $ flip onButton True
-                        StickMove t (V2 x y) -> lookup' stickMap t \(x', y') -> onAxis x' x >> onAxis y' y
-                        SliderMove t x -> lookup' sliderMap t $ flip onAxis $ x * 2 - 1
-                    pure True
+                        onUpdate u <> case u of
+                            ServerUpdate su -> [] <$ case su of
+                                SetLayout l -> put (l, mkElementMaps l.elements, s)
+                                SwitchLayout i -> asks ((!? i) . fst3) >>= \case
+                                    Just l -> put (l, mkElementMaps l.elements, s)
+                                    Nothing -> warn $ "layout id not found: " <> i.unwrap
+                                AddElement el -> modify . first $ addToElementMaps el
+                                RemoveElement el -> modify . first $
+                                    over #stickMap (Map.delete el)
+                                        . over #sliderMap (Map.delete el)
+                                        . over #buttonMap (Map.delete el)
+                                _ -> mempty
+                            ClientUpdate cu -> do
+                                ElementMaps{..} <- gets snd3
+                                case cu of
+                                    ButtonUp t -> lookup' buttonMap t $ flip onButton False
+                                    ButtonDown t -> lookup' buttonMap t $ flip onButton True
+                                    StickMove t (V2 x y) -> lookup' stickMap t \(x', y') -> onAxis x' x >> onAxis y' y
+                                    SliderMove t x -> lookup' sliderMap t $ flip onAxis $ x * 2 - 1
+                              where
+                                lookup' m t f = case m !? t of
+                                    Just b -> f b
+                                    Nothing -> warn ("element id not found: " <> t.unwrap) >> mempty
                   where
-                    lookup' m t f = case m !? t of
-                        Just b -> f b
-                        Nothing -> warn ("element id not found: " <> t.unwrap) >> mempty
-                Right (Left err) -> do
-                    handleUpdates =<< onDroppedConnection err
-                    pure False
+                    sus = us & mapMaybe \case
+                            ServerUpdate s -> Just s
+                            ClientUpdate _ -> Nothing
+        WS.withPingThread conn pingFrequency onPing
+            . (=<<) (either (flip (onDroppedConnection clientId) e) pure)
+            . runMonpad layouts clientId e s0
+            . SP.drain
+            $ SP.mapM handleUpdates allUpdates
       where
         sendUpdates conn = liftIO . WS.sendTextData conn . encode
-        getUpdate conn = liftIO $ try (WS.receiveData conn) <&> \case
-            Left err -> Left $ WebSocketException err
-            Right b -> first UpdateDecodeException $ eitherDecode b
+        getUpdate conn = liftIO (try $ WS.receiveData conn) >>= \case
+            Left err -> throwError $ WebSocketException err
+            Right b -> case eitherDecode b of
+                Left err -> throwError $ UpdateDecodeException err
+                Right x -> pure x
 
 {- | Auto generate Elm datatypes, encoders/decoders etc.
 It's best to run this via GHCI or HLS.
@@ -406,7 +416,7 @@ e.g. if we added an extra case to 'Update', it would need to be handled in vario
 -- >>> elm "elm"
 elm :: FilePath -> IO ()
 elm pathToElm = Elm.writeDefs pathToElm $ mconcat
-    [ Elm.decodedTypes @Update
+    [ Elm.decodedTypes @ClientUpdate
     , Elm.decodedTypes @(V2 Double)
     , Elm.encodedTypes @(ServerUpdate () ())
     , Elm.encodedTypes @ResetLayout

elm/src/Auto/ClientUpdate.elm
@@ -1,18 +1,18 @@
-module Auto.Update exposing (..)
+module Auto.ClientUpdate exposing (..)
 
 import Json.Encode
 import Math.Vector2
 import Util
 
 
-type Update 
+type ClientUpdate 
     = ButtonUp String
     | ButtonDown String
     | StickMove String Math.Vector2.Vec2
     | SliderMove String Float
 
 
-encode : Update -> Json.Encode.Value
+encode : ClientUpdate -> Json.Encode.Value
 encode a =
     case a of
         ButtonUp b ->

elm/src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Auto.Button exposing (..)
+import Auto.ClientUpdate exposing (..)
 import Auto.Colour exposing (..)
 import Auto.Element as Element
 import Auto.ElmFlags exposing (..)
@@ -16,7 +17,6 @@ import Auto.Slider exposing (..)
 import Auto.Stick exposing (..)
 import Auto.TextBox exposing (..)
 import Auto.TextStyle exposing (..)
-import Auto.Update exposing (..)
 import Auto.ViewBox exposing (..)
 import Basics exposing (..)
 import Basics.Extra exposing (..)
@@ -276,7 +276,7 @@ viewButton name button pressed =
             )
         |> onPointerDown (always <| ButtonDown name)
             { onMove = always []
-            , onRelease = [ Update <| ButtonUp name ]
+            , onRelease = [ ClientUpdate <| ButtonUp name ]
             }
 
 
@@ -308,8 +308,8 @@ viewStick name stick toOffset stickPos =
     big
         |> impose (small |> shift (unVec2 <| Vec2.scale range stickPos))
         |> onPointerDown (StickMove name << getOffset)
-            { onMove = \event -> [ Update <| StickMove name <| getOffset event ]
-            , onRelease = [ Update <| StickMove name <| vec2 0 0 ]
+            { onMove = \event -> [ ClientUpdate <| StickMove name <| getOffset event ]
+            , onRelease = [ ClientUpdate <| StickMove name <| vec2 0 0 ]
             }
 
 
@@ -347,10 +347,10 @@ viewSlider name slider toOffset pos =
     background
         |> impose (stick |> shift (unVec2 <| Vec2.scale pos v))
         |> onPointerDown (SliderMove name << getOffset)
-            { onMove = \event -> [ Update <| SliderMove name <| getOffset event ]
+            { onMove = \event -> [ ClientUpdate <| SliderMove name <| getOffset event ]
             , onRelease =
                 if slider.resetOnRelease then
-                    [ Update <| SliderMove name <| slider.initialPosition ]
+                    [ ClientUpdate <| SliderMove name <| slider.initialPosition ]
 
                 else
                     []
@@ -517,7 +517,7 @@ type alias Msgs =
 
 
 type Msg
-    = Update Update
+    = ClientUpdate ClientUpdate
     | ServerUpdate ServerUpdate
     | PointerDown Int PointerCallbacks
     | PointerUp Pointer.Event
@@ -587,7 +587,7 @@ update msg model =
             model.layout
     in
     case msg of
-        Update u ->
+        ClientUpdate u ->
             let
                 model1 =
                     case u of
@@ -890,7 +890,7 @@ subLogErrors s f =
 
 
 onPointerDown :
-    (Pointer.Event -> Update)
+    (Pointer.Event -> ClientUpdate)
     -> PointerCallbacks
     -> Collage Msgs
     -> Collage Msgs
@@ -900,7 +900,7 @@ onPointerDown f y =
             |> JD.map
                 (\x ->
                     [ PointerDown x.pointerId y
-                    , Update <| f x
+                    , ClientUpdate <| f x
                     ]
                 )
         )

elm/src/Ports.elm
@@ -13,7 +13,7 @@ port module Ports exposing
 -}
 
 import Auto.ServerUpdate exposing (ServerUpdate)
-import Auto.Update exposing (Update)
+import Auto.ClientUpdate exposing (ClientUpdate)
 import Json.Decode as JD
 import Json.Encode as JE
 
@@ -37,9 +37,9 @@ port fullscreenChangePort :
     -> Sub msg
 
 
-sendUpdate : Update -> Cmd msg
+sendUpdate : ClientUpdate -> Cmd msg
 sendUpdate =
-    sendUpdatePort << Auto.Update.encode
+    sendUpdatePort << Auto.ClientUpdate.encode
 
 
 port sendUpdatePort :

haskell/app/Main.hs
@@ -121,12 +121,12 @@ main = do
         Just wsPort -> serverExtWs @() @() (maybe mempty writeQR qrPath) port wsPort loginImageUrl assetsDir
             =<< layoutsFromDhall dhallLayouts
           where
-            writeQR path url = withPlugin (QR.plugin path) \sc -> onStart (sc $ error "QR plugin ignores layouts") url
+            writeQR path url = withPlugin (QR.plugin path) $ flip onStart url
         Nothing -> if systemDevice
             then run OS.keyUnknown OS.conf =<< layoutsFromDhall dhallLayouts
             else run @() @() @Unit @Unit mempty mempty =<< layoutsFromDhall dhallLayouts
           where
-            run :: forall e s a b. (Monoid e, Monoid s, FromDhall a, FromDhall b) =>
+            run :: forall e s a b. (Monoid e, Monoid s, FromDhall a, FromDhall b, Show a, Show b) =>
                 b -> ServerConfig e s a b -> Layouts a b -> IO ()
             --TODO with `ImpredicativeTypes`, we can use 'flip withPlugin' (and/or go point-free?)
             run unknown sc ls = withPlugin ps $ server pingFrequency port loginImageUrl assetsDir ls

haskell/monpad.cabal
@@ -120,6 +120,7 @@ library
         colour ^>= 2.3.5,
         convertible ^>= 1.1.1,
         elm-syntax ^>= 0.3.0,
+        exceptions ^>= 0.10.4,
         generic-data ^>= 0.9.2,
         generic-functor ^>= 0.2,
         generics-sop ^>= 0.5.0,
@@ -128,6 +129,7 @@ library
         http-types ^>= 0.12.3,
         JuicyPixels ^>= 3.3.5,
         lucid ^>= 2.9.12,
+        monad-control ^>= 1.0.2.3,
         network ^>= 3.1.1.1,
         prettyprinter ^>= 1.7.0,
         prizm ^>= 3.0.0,
@@ -139,6 +141,7 @@ library
         servant-server ^>= 0.18,
         servant-websockets ^>= 2.0,
         streamly-fsnotify ^>= 1.1.1.0,
+        transformers-base ^>= 0.4.5.2,
         wai ^>= 3.2.2,
         warp ^>= 3.3.9,
         websockets ^>= 0.12.7,

haskell/src/Monpad/Plugins.hs
@@ -19,9 +19,9 @@ plugins = foldl1 \(Plugin x) (Plugin y) -> Plugin $ combineConfs x y
 
 onLayoutChange ::
     (Layout a b -> Monpad e s a b [ServerUpdate a b]) ->
-    ServerUpdate a b ->
+    Update a b ->
     Monpad e s a b [ServerUpdate a b]
 onLayoutChange f = \case
-    SwitchLayout i -> asks (Map.lookup i . fst3) >>= maybe mempty f
-    SetLayout l -> f l
+    ServerUpdate (SwitchLayout i) -> asks (Map.lookup i . fst3) >>= maybe mempty f
+    ServerUpdate (SetLayout l) -> f l
     _ -> mempty

haskell/src/Monpad/Plugins/LayoutSwitcher.hs
@@ -18,8 +18,8 @@ plugin :: b -> Plugin a b
 plugin = Plugin . switcher @()
 
 switcher :: Monoid e => b -> ServerConfig e (Stream ((LayoutID, ViewBox), Bool)) a b
-switcher buttonData (fmap ((.name) &&& (.viewBox)) -> ls) =
-    let onNewConnection = const $ pure
+switcher buttonData =
+    let onNewConnection = \(fmap ((.name) &&& (.viewBox)) -> ls) _ -> pure
             ( mempty
             , Stream.prepend
                 (zip (toList ls) (repeat True))
@@ -54,8 +54,8 @@ switcher buttonData (fmap ((.name) &&& (.viewBox)) -> ls) =
             { onNewConnection
             , updates = mempty
             , onStart = mempty
-            , onMessage = \case
-                ButtonUp t | t == elementId -> do
+            , onUpdate = \case
+                ClientUpdate (ButtonUp t) | t == elementId -> do
                     modify $ third3 Stream.tail
                     ((l, vb), new) :> _ <- gets thd3
                     pure $
@@ -66,5 +66,4 @@ switcher buttonData (fmap ((.name) &&& (.viewBox)) -> ls) =
             , onButton = mempty
             , onDroppedConnection = mempty
             , onPong = mempty
-            , onUpdate = mempty
             }

commit e58a6c083082a8dc9963e6a72351c75d4872555d

Stop pong handler blocking all updates
Regression introduced in 2b71718.

haskell/src/Monpad.hs
@@ -345,8 +345,8 @@ websocketServer pingFrequency layouts ServerConfig{..} mu pending0 = liftIO case
         conn <- WS.acceptRequest pending
         let allUpdates = asyncly $ (pure . ClientUpdate <$> clientUpdates) <> (ServerUpdate <<$>> serverUpdates)
             clientUpdates = serially . SP.repeatM $ getUpdate conn
-            serverUpdates = serially . SP.mapM (gets (view #extra) <&>) . SP.hoist liftIO $
-                SP.cons (const u0) (asyncly $ updates e) <> SP.repeatM (const <$> takeMVar extraUpdates)
+            serverUpdates = serially . SP.mapM (gets (view #extra) <&>) . SP.hoist liftIO . asyncly $
+                SP.cons (const u0) (updates e) <> SP.repeatM (const <$> takeMVar extraUpdates)
             handleUpdates = sendUpdates conn . map (bimap (const Unit) (const Unit)) . concat <=< traverse (go . pure)
               where
                 go us = if null us
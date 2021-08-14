b7a6277b78528af122d8119c90d5bca50f58b02e
George Thomas <georgefsthomas@gmail.com>
2021-08-14 12:37:35 +0100

    Make it easier to use plugins in Test.hs

--- haskell/src/Test.hs
+++ haskell/src/Test.hs
@@ -5,7 +5,11 @@ import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Text (Text)
 import Data.Text.IO qualified as T
 import GHC.IO.Encoding (setLocaleEncoding)
+import Monpad.Plugins.LayoutSwitcher qualified as LayoutSwitcher
 import Monpad.Plugins.Logger qualified as Logger
+import Monpad.Plugins.PingIndicator qualified as PingIndicator
+import Monpad.Plugins.QR qualified as QR
+import Monpad.Plugins.WatchLayout qualified as WatchLayout
 import System.IO (utf8)
 
 import Monpad
@@ -14,18 +18,31 @@ import Monpad.Plugins
 defaultSimple :: Text
 defaultSimple = "(../dhall/lib/map-layout.dhall).void ../dhall/default.dhall"
 
-test :: [Plugin () ()] -> [Text] -> IO ()
+test :: [P] -> [Text] -> IO ()
 test ps ls = do
     setLocaleEncoding utf8
     Just layouts <- layoutsFromDhall $ defaultSimple :| ls
     withPlugin
-        (plugins $ Logger.plugin T.putStrLn Logger.Loud : ps)
+        (plugins $ Logger.plugin T.putStrLn Logger.Loud : map plugin ps)
         $ server
             30
             8000
             Nothing
             (Just "../dist/assets")
             layouts
+
+-- NB: this cover all plugins except 'Logger', which is always in use here.
+data P
+    = WatchLayout
+    | QR FilePath
+    | LayoutSwitcher
+    | PingIndicator
+plugin :: P -> Plugin () ()
+plugin = \case
+    WatchLayout -> WatchLayout.plugin
+    QR path -> QR.plugin path
+    LayoutSwitcher -> LayoutSwitcher.plugin ()
+    PingIndicator -> PingIndicator.plugin
 
 testExt :: IO ()
 testExt = do

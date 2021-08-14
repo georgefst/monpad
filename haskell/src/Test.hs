1e0e5995715d619396ab4940aebcc3c47ad80205
George Thomas <georgefsthomas@gmail.com>
2021-08-14 12:36:19 +0100

    Use logging plugin in Test.hs
    Avoids duplicated functionality code, and gives cleaner output that's less reliant on pretty-simple.
    
    Some of the output here did previously use pretty-simple in compact mode, but it shouldn't make that much difference.

--- haskell/src/Test.hs
+++ haskell/src/Test.hs
@@ -1,14 +1,12 @@
 -- | Stuff for quickly playing around in GHCI. Call 'runghc Build.hs assets' before using these.
 module Test where
 
-import Control.Monad.Reader
-import Optics
-import System.IO
-import Text.Pretty.Simple
-
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Text (Text)
+import Data.Text.IO qualified as T
 import GHC.IO.Encoding (setLocaleEncoding)
+import Monpad.Plugins.Logger qualified as Logger
+import System.IO (utf8)
 
 import Monpad
 import Monpad.Plugins
@@ -21,27 +19,13 @@ test ps ls = do
     setLocaleEncoding utf8
     Just layouts <- layoutsFromDhall $ defaultSimple :| ls
     withPlugin
-        (plugins $ Plugin config : ps)
+        (plugins $ Logger.plugin T.putStrLn Logger.Loud : ps)
         $ server
             30
             8000
             Nothing
             (Just "../dist/assets")
             layouts
-  where
-    config =
-        mempty
-            { onStart = pPrint . ("started" :: Text,)
-            , onNewConnection = \_ c -> do
-                pPrint ("connected" :: Text, c)
-                pure ((), (), [])
-            , onUpdate = \u -> do
-                c <- asks $ view #client
-                pPrintOpt NoCheckColorTty defaultOutputOptionsDarkBg{outputOptionsCompact = True} (c, u)
-                mempty
-            , onDroppedConnection = curry $ const . (>> mempty) . pPrint . ("disconnected" :: Text,)
-            , onPong = const $ (mempty <$) . pPrint . ("pong" :: Text,)
-            }
 
 testExt :: IO ()
 testExt = do

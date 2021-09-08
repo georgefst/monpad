2b2e7525293f6d7646ca0e4d3d54815cd72e49b6
George Thomas <georgefsthomas@gmail.com>
2021-09-09 00:15:37 +0100

    Just use Desktop for QR path in test implementation
    For one thing, this means we can use `test Data.List.Extra.enumerate []` to run with all plugins enabled.

--- haskell/src/Test.hs
+++ haskell/src/Test.hs
@@ -1,6 +1,9 @@
 -- | Stuff for quickly playing around in GHCI. Call 'runghc Build.hs assets' before using these.
 module Test where
 
+import System.Directory.Extra
+import System.FilePath
+
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Text (Text)
 import Data.Text.IO qualified as T
@@ -11,6 +14,7 @@ import Monpad.Plugins.PingIndicator qualified as PingIndicator
 import Monpad.Plugins.QR qualified as QR
 import Monpad.Plugins.WatchLayout qualified as WatchLayout
 import System.IO (utf8)
+import System.IO.Unsafe (unsafeInterleaveIO)
 
 import Monpad
 import Monpad.Plugins
@@ -21,22 +25,24 @@ defaultSimple = "(../dhall/lib/map-layout.dhall).void ../dhall/textinput.dhall"
 -- NB: this covers all plugins except 'Logger', which is always in use here.
 data P
     = WL
-    | QR FilePath
+    | QR
     | LS
     | PI
-plugin :: P -> Plugin () ()
-plugin = \case
+    deriving (Eq, Enum, Bounded)
+plugin :: FilePath -> P -> Plugin () ()
+plugin home = \case
     WL -> WatchLayout.plugin
-    QR path -> QR.plugin path
+    QR -> QR.plugin $ home </> "Desktop"
     LS -> LayoutSwitcher.plugin ()
     PI -> PingIndicator.plugin
 
 test :: [P] -> [Text] -> IO ()
 test ps ls = do
+    home <- unsafeInterleaveIO getHomeDirectory
     setLocaleEncoding utf8
     Just layouts <- layoutsFromDhall $ defaultSimple :| ls
     withPlugin
-        (plugins $ Logger.plugin T.putStrLn Logger.Loud : map plugin ps)
+        (plugins $ Logger.plugin T.putStrLn Logger.Loud : map (plugin home) ps)
         $ server
             30
             8000

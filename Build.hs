georgefst/monpad

commit 01fe4e0ee47c1d82ce5d5c469485874344772e3e

Fix debug target
Side effect is that symlinks in `haskell/rsc` are now absolute (regardless of `debug`).

Build.hs
@@ -52,15 +52,8 @@ rules = do
 
     forM_ linkedAssets \(file, link) ->
         link %> \_ -> do
-            let copy = liftIO $ Dir.copyFile file link
             need [file]
-            liftIO (Dir.doesFileExist link) >>= \case
-                True -> unlessM (liftIO $ Dir.pathIsSymbolicLink link) copy
-                False -> do
-                    noLinkPermission <- liftIO do
-                        (Dir.createFileLink (".." </> ".." </> file) link >> pure False)
-                            `catchPermissionError` \_ -> pure True
-                    when noLinkPermission $ putWarn "No permission to create symbolic links - copying instead" >> copy
+            trySymlink file link
 
     let haskell exeName path flags = do
             need assets
@@ -100,7 +93,11 @@ rules = do
             _ <- Dhall.throws $ Dhall.typeOf resolvedExpression
             T.writeFile out $ Dhall.pretty resolvedExpression
 
-    "debug" ~> haskell "monpad" ("dist" </> "monpad-debug" <.> exe) "" --unoptimised, non-portable
+    --unoptimised, and needs to be positioned relative to `rsc`
+    "debug" ~> do
+        haskell "monpad" ("dist" </> "monpad-debug" <.> exe) ""
+        trySymlink rscDir "dist/rsc"
+
     "elm" ~> need [elmJS]
     "elm-debug" ~> elm ""
     "dhall" ~> do
@@ -174,3 +171,16 @@ bracketed t = pack "(" <> t <> pack ")"
 
 catchPermissionError :: IO a -> (IOError -> IO a) -> IO a
 catchPermissionError = catchBool isPermissionError
+
+-- | If we can't symlink due to permissions (e.g. Windows non-admin shell), fall back to copying.
+trySymlink :: FilePath -> FilePath -> Action ()
+trySymlink file link = do
+    let copy = liftIO $ Dir.copyFile file link
+    file' <- liftIO $ Dir.makeAbsolute file
+    liftIO (Dir.doesPathExist link) >>= \case
+        True -> unlessM (liftIO $ Dir.pathIsSymbolicLink link) copy
+        False -> do
+            noLinkPermission <- liftIO do
+                (Dir.createFileLink file' link >> pure False)
+                    `catchPermissionError` \_ -> pure True
+            when noLinkPermission $ putWarn "No permission to create symbolic links - copying instead" >> copy

commit dcb0454847de2e6a40a614ebcf25207a7ffc7ef0

Fix build script dependencies
This will have been broken since 01fe4e0. I've only caught it because I'm actually running it as a Cabal script, to extract the environment file on my new laptop. Rather than using `cabal install --lib`, which can omit `directory` since it ships with GHC.

Build.hs
@@ -7,6 +7,7 @@
 build-depends:
     base >= 4.13,
     dhall ^>= 1.39,
+    directory ^>= 1.3.7.0,
     extra ^>= 1.7.4,
     language-javascript ^>= 0.7.1.0,
     pretty-simple ^>= 4.0,
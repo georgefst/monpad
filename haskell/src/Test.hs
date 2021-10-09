aec84ffd0969a505906fd7a8e7d276ab1c4632ea
George Thomas <georgefsthomas@gmail.com>
2021-10-09 14:22:39 +0100

    fsnotify experiments
    See https://github.com/haskell-fswatch/hfsnotify/issues/102.

--- haskell/src/Test.hs
+++ haskell/src/Test.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 -- | Stuff for quickly playing around in GHCI. Call 'runghc Build.hs assets' before using these.
 module Test where
 
@@ -19,6 +21,15 @@ import System.IO.Unsafe (unsafeInterleaveIO)
 
 import Monpad
 import Monpad.Plugins
+
+import Control.Concurrent
+import Control.Exception
+import Control.Monad
+import Streamly.FSNotify (EventPredicate (EventPredicate), watchDirectoryWith)
+import Streamly.Prelude qualified as SP
+import System.FSNotify
+import System.INotify
+import Text.Pretty.Simple
 
 dhallLayoutDefault :: Text
 dhallLayoutDefault = "../dhall/textinput.dhall"
@@ -65,3 +76,26 @@ testExt = do
         Nothing
         (Just "../dist/assets")
         layouts
+
+-- conf = defaultConfig
+-- conf = defaultConfig{confDebounce = Debounce 0.1}
+conf = defaultConfig{confDebounce = NoDebounce}
+
+dir = "../dhall"
+
+p mv x = takeMVar mv >> pPrint x >> putMVar mv ()
+
+fs, fss, hi :: IO ()
+fs = withManagerConf conf \mgr -> do
+    mv <- newMVar ()
+    _ <- watchDir mgr dir (const True) (p mv)
+    forever $ threadDelay maxBound
+fss = do
+    mv <- newMVar ()
+    SP.mapM_ (p mv) . snd =<< watchDirectoryWith conf dir (EventPredicate $ const True)
+hi = do
+    mv <- newMVar ()
+    inot <- initINotify
+    wd <- addWatch inot [AllEvents] dir (p mv)
+    threadDelay maxBound
+        `catch` \(e :: SomeException) -> print e >> removeWatch wd

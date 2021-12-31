georgefst/monpad

commit f41a8cb8d1340e9a852b872eccd62b53d06ee975

Call `list-bin` with the same arguments as `build`
This ensures that we find the right binary.

Right now `--builddir` is the only one that actually matters.

Build.hs
@@ -59,17 +59,20 @@ rules = do
     let haskell exeName path flags = do
             need assets
             needDirExcept hsBuildDir hsDir
+            let args =
+                    [ "exe:" <> exeName
+                    , "--flags=" <> flags
+                    , "--builddir=" <> (".." </> hsBuildDir)
+                    ]
             cmd_
                 (Cwd hsDir)
                 "cabal build"
-                ("exe:" <> exeName)
-                ("--flags=" <> flags)
-                ("--builddir=" <> (".." </> hsBuildDir))
             bins <-
                 lines . fromStdout
                     <$> cmd
                         (Cwd hsDir)
-                        "cabal list-bin ."
+                        "cabal list-bin"
+                        args
             case bins of
                 [] -> error "No matches"
                 [path'] -> copyFileChanged path' path

commit 61d8bd0c9e9e6661ff6c1b2fedb178523e43e0d2

Pass cabal args to build command
Fixes serious regression in f41a8cb.

Build.hs
@@ -68,6 +68,7 @@ rules = do
             cmd_
                 (Cwd hsDir)
                 "cabal build"
+                args
             bins <-
                 lines . fromStdout
                     <$> cmd
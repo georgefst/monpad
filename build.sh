dec472f67ed7e5ce3ca3c1ffda3857360d60548c
George Thomas <georgefsthomas@gmail.com>
2022-02-25 12:03:04 +0000

    Correct build dir for build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # this won't be necessary once all dependencies have updated for 9.2
 # and it wouldn't be necessary if we could use `source-repository-package`, `allow-newer` etc. in cabal scripts...
-cabal run Build.hs --project-file haskell/cabal.project --builddir .build --write-ghc-environment-files=always
+cabal run Build.hs --project-file haskell/cabal.project --builddir .build/hs --write-ghc-environment-files=always

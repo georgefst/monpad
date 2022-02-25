585129d382c76c4d93ecbcf915e915d72ce2cdaa
George <you@example.com>
2022-02-25 12:55:55 +0000

    Simplify build wrapper
    Turns out we can use local cabal.project if we make local packages optional.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 # this won't be necessary once all dependencies have updated for 9.2
 # and it wouldn't be necessary if we could use `source-repository-package`, `allow-newer` etc. in cabal scripts...
-cat haskell/cabal.project | tail -n +2 > /tmp/cabal.project.build
-cabal run Build.hs --project-file /tmp/cabal.project.build --builddir .build --write-ghc-environment-files=always
+cabal run Build.hs --project-file haskell/cabal.project --builddir .build --write-ghc-environment-files=always

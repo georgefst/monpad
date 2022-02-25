51277cf326088f89b009bb242ec09e6fe73b5f21
George Thomas <georgefsthomas@gmail.com>
2022-02-25 14:57:44 +0000

    Silence stderr from compile check

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ghc -fno-code Build.hs ; then
+if ghc -fno-code Build.hs 2> /dev/null ; then
     ghc --run Build.hs
 else
     cabal run Build.hs --project-file haskell/cabal.project --builddir .build/hs --write-ghc-environment-files=always

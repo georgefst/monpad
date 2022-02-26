b7fc8b25b739ac52e0d69a5a7932987575d9cf09
George Thomas <georgefsthomas@gmail.com>
2022-02-26 15:31:26 +0000

    Add Windows build script

---
+++ build.ps1
@@ -0,0 +1,37 @@
+$progs =
+[pscustomobject]@{
+    prog     = "ghc"
+    required = "9.2"
+},
+[pscustomobject]@{
+    prog     = "cabal"
+    required = "3.6"
+},
+[pscustomobject]@{
+    prog     = "elm"
+    required = "0.19"
+}
+
+foreach ($prog in $progs) {
+    $installed = ((
+            Invoke-expression ($prog.prog + ' --version')
+        ).Split([Environment]::NewLine) |
+        Select-Object -First 1
+    ).Split(' ') |
+    Select-Object -Last 1
+    if ([version]$prog.required -gt [version]$installed) {
+        Write-Host "$($prog.prog) version $($prog.required) required - $installed found"
+        exit 1
+    }
+}
+
+#TODO this is a workaround for https://github.com/haskell/cabal/issues/7083
+ghc --run windows-cabal-project-hack.hs
+
+ghc -fno-code Build.hs 2>&1 | out-null
+if ($?) {
+    ghc --run Build.hs
+}
+else {
+    cabal run Build.hs --project-file haskell/cabal.project.patched --builddir .build/hs --write-ghc-environment-files=always
+}
---
+++ windows-cabal-project-hack.hs
@@ -0,0 +1,19 @@
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+
+main = do
+    let badPkgs = map T.pack ["rawfilepath"]
+        bad = any $ \t0 ->
+            let t = T.strip t0
+             in T.pack "location" `T.isPrefixOf` t && any (`T.isSuffixOf` t) badPkgs
+    T.writeFile "haskell\\cabal.project.patched"
+        . T.unlines
+        . map T.unlines
+        . filter (not . bad)
+        . split T.null
+        . T.lines
+        =<< T.readFile "haskell\\cabal.project"
+  where
+    split _ [] = [[]]
+    split f (x : xs) | f x = [] : split f xs
+    split f (x : xs) | y : ys <- split f xs = (x : y) : ys

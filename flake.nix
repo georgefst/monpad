591c454efc1d5f2357dac62810a0604e25ac8801
George Thomas <georgefsthomas@gmail.com>
2022-08-12 14:10:59 +0100

    Build fully statically with musl
    Taken from https://input-output-hk.github.io/haskell.nix/tutorials/cross-compilation.html#static-executables-with-musl-libc.

--- flake.nix
+++ flake.nix
@@ -11,12 +11,25 @@
           haskellNix.overlay
           (final: prev: {
             myHaskellProject =
-              final.haskell-nix.cabalProject' {
+              final.haskell-nix.cabalProject' ({
                 src = ./haskell;
                 compiler-nix-name = "ghc924";
                 shell.tools = { cabal = { }; };
                 shell = { inherit crossPlatforms; };
-              };
+              }
+               // (if pkgs.stdenv.hostPlatform.isMusl then
+                  {
+                    configureArgs =
+                      ''
+                        --disable-executable-dynamic
+                        --disable-shared
+                        --ghc-option=-optl=-pthread
+                        --ghc-option=-optl=-static
+                        --ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib
+                        --ghc-option=-optl=-L${pkgs.zlib.static}/lib
+                      '';
+                  } else { })
+                );
           })
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };

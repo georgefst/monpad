a803c747e6e6145fd9dee2d26b62031b251e6550
George Thomas <georgefsthomas@gmail.com>
2022-08-12 13:08:21 +0100

    Add basic generic Nix flake config
    Largely taken from https://input-output-hk.github.io/haskell.nix/tutorials/getting-started-flakes.html.
    
    Builds from x86 Linux, for x86 Musl Linux or Windows.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "monpad";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        crossPlatforms = p: [ p.mingwW64 p.musl64 ];
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            myHaskellProject =
+              final.haskell-nix.cabalProject' {
+                src = ./haskell;
+                compiler-nix-name = "ghc924";
+                shell.tools = { cabal = { }; };
+                shell = { inherit crossPlatforms; };
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      in
+      pkgs.myHaskellProject.flake { inherit crossPlatforms; });
+}

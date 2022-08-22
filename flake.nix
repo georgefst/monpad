6d7421f54607a389857ce0dab59124d00efd1461
George Thomas <georgefsthomas@gmail.com>
2022-08-23 00:47:50 +0100

    Remove unnecessary Musl configure args

--- flake.nix
+++ flake.nix
@@ -16,20 +16,7 @@
                 compiler-nix-name = "ghc924";
                 shell.tools = { cabal = { }; };
                 shell = { inherit crossPlatforms; };
-              }
-              // (if pkgs.stdenv.hostPlatform.isMusl then
-                {
-                  configureArgs =
-                    ''
-                      --disable-executable-dynamic
-                      --disable-shared
-                      --ghc-option=-optl=-pthread
-                      --ghc-option=-optl=-static
-                      --ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib
-                      --ghc-option=-optl=-L${pkgs.zlib.static}/lib
-                    '';
-                } else { })
-              );
+              });
           })
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };

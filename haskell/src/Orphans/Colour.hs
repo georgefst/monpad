88da91921f502704c05518b4a94f2dabe470e35d
George Thomas <georgefsthomas@gmail.com>
2022-11-25 01:23:06 +0000

    refactor: Avoid need for `Via'` in `Colour'`

--- haskell/src/Orphans/Colour.hs
+++ haskell/src/Orphans/Colour.hs
@@ -5,7 +5,8 @@ module Orphans.Colour () where
 import Data.Aeson (ToJSON (toJSON))
 import Data.Aeson qualified as J
 import Data.Bifunctor (first)
-import Data.Colour (AlphaColour, Colour, alphaChannel, black, over, withOpacity)
+import Data.Colour (AlphaColour, alphaChannel, black, over, withOpacity)
+import Data.Colour qualified as Colour
 import Data.Colour.SRGB (RGB (channelBlue, channelGreen, channelRed), sRGB, sRGB24reads, toSRGB)
 import Data.Text qualified as T
 import Deriving.Aeson (CustomJSON (CustomJSON))
@@ -21,7 +22,7 @@ instance ToJSON (AlphaColour Double) where
     toJSON =
         J.toJSON . \c ->
             let rgb = toSRGB $ c `over` black
-             in Colour'
+             in Colour
                     { red = rgb.channelRed
                     , green = rgb.channelGreen
                     , blue = rgb.channelBlue
@@ -29,31 +30,31 @@ instance ToJSON (AlphaColour Double) where
                     }
 
 instance FromDhall (AlphaColour Double) where
-    autoWith = fmap (\(c :: Colour') -> withOpacity (sRGB c.red c.green c.blue) c.alpha) . autoWith
+    autoWith = fmap (\(c :: Colour) -> withOpacity (sRGB c.red c.green c.blue) c.alpha) . autoWith
 
 instance HasElmDecoder J.Value (AlphaColour Double) where
-    elmDecoderDefinition = elmDecoderDefinition @J.Value @Colour'
+    elmDecoderDefinition = elmDecoderDefinition @J.Value @Colour
 instance HasElmType (AlphaColour Double) where
-    elmDefinition = elmDefinition @Colour'
+    elmDefinition = elmDefinition @Colour
 
-data Colour' = Colour'
+data Colour = Colour
     { red :: Double
     , green :: Double
     , blue :: Double
     , alpha :: Double
     }
     deriving (Show, Generic, FromDhall, SOP.Generic, SOP.HasDatatypeInfo)
-    deriving (ToJSON) via CustomJSON Opts.JSON Colour'
-    deriving (HasElmType, HasElmDecoder J.Value) via Elm.Via' "Colour" Colour'
+    deriving (ToJSON) via CustomJSON Opts.JSON Colour
+    deriving (HasElmType, HasElmDecoder J.Value) via Elm.Via Colour
 
-instance FromHttpApiData (Colour Float) where
+instance FromHttpApiData (Colour.Colour Float) where
     parseUrlPiece = first T.pack . readEither' . T.unpack
 
 {- TODO there should be a better way
 https://www.reddit.com/r/haskell/comments/ufrk6a/comment/i9c4sg9/?utm_source=share&utm_medium=web2x&context=3
 -}
-readEither' :: String -> Either String (Colour Float)
+readEither' :: String -> Either String (Colour.Colour Float)
 readEither' = fmap (\(ReadWrapper c) -> c) . readEither @ReadWrapper
-newtype ReadWrapper = ReadWrapper (Colour Float)
+newtype ReadWrapper = ReadWrapper (Colour.Colour Float)
 instance Read ReadWrapper where
     readsPrec _ = map (first ReadWrapper) . sRGB24reads @Float

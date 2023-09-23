5bf3f5234eba692c652e9dd5cae168195d10d130
George Thomas <georgefsthomas@gmail.com>
2023-09-23 13:19:01 +0100

    Better error handling in opts file attribute lookup with repect to falsiness

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const attrs = document.currentScript.attributes
 // TODO not supported - https://github.com/erikd/language-javascript/issues/71
 // const optsFilePath = attrs.getNamedItem("optsFile")?.value
-const optsFilePath = attrs.getNamedItem("optsFile") ? attrs.getNamedItem("optsFile").value : undefined
-const optsFilePromise = optsFilePath ? fetch(optsFilePath).then(r => r.json()).catch(_ => ({})) : Promise.resolve({})
+const optsFilePath = attrs.getNamedItem("optsFile") != undefined ? attrs.getNamedItem("optsFile").value : undefined
+const optsFilePromise = optsFilePath != undefined
+    ? fetch(optsFilePath).then(r => r.json()).catch(_ => ({}))
+    : Promise.resolve({})
 optsFilePromise.then(fileOpts => {
 
-const attr = s => fileOpts[s] || attrs.getNamedItem(s).value
+const attr = s => fileOpts[s] ?? attrs.getNamedItem(s).value
 
 const params = new URLSearchParams(window.location.search)
 const username = params.get("username")
